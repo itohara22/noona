0a568323b595d9032c558189f640711603eca4fb
ito <ito.k23018@gmail.com>
2025-12-13 20:01:01 +0530

    added pointer i in decode string

--- main.go
+++ main.go
@@ -10,31 +10,31 @@ func main() {
 	// dic := "d3:cow3:moo4:spam4:eggsi42el4:test3:one3:twoe3:fooi99ee"
 	// decodeDict(dic)
 	strrr := "4:spam3:egg"
-	for _, s := range decodeString(strrr) {
-		fmt.Println(s)
+	i := 0
+	res := ""
+	for i < len(strrr) {
+		res += decodeString(strrr, &i)
 	}
+	fmt.Println(res)
 }
 
-func decodeString(s string) []string { // Implemnet chr counter to go over the whole bencode *i
-	res := []string{}
-	i := 0
-	for i < len(s) {
-		r := rune(s[i])
-		if r > '0' && r < '9' {
-			length, err := strconv.Atoi(string(r))
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-			i++
-			if s[i] != ':' {
-				log.Fatal("expected colon")
-			}
-			i++
-			strParsed := s[i : i+length]
-			res = append(res, strParsed)
-		} else {
-			i++
+func decodeString(s string, i *int) string {
+	res := ""
+	r := rune(s[*i])
+	if r > '0' && r < '9' {
+		length, err := strconv.Atoi(string(r))
+		if err != nil {
+			log.Fatal(err.Error())
 		}
+		*i++
+		if s[*i] != ':' {
+			log.Fatal("expected colon")
+		}
+		*i++
+		strParsed := s[*i : *i+length]
+		res += strParsed
+	} else {
+		*i++
 	}
 	return res
 }
