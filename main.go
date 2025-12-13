67ccf911bdcad7f8739bd6fa18e1ba46c1251e40
ito <ito.k23018@gmail.com>
2025-12-13 20:15:11 +0530

    decode string

--- main.go
+++ main.go
@@ -4,18 +4,40 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"unicode"
 )
 
+// dic := "d3:cow3:moo4:spam4:eggsi42el4:test3:one3:twoe3:fooi99ee"
 func main() {
-	// dic := "d3:cow3:moo4:spam4:eggsi42el4:test3:one3:twoe3:fooi99ee"
-	// decodeDict(dic)
-	strrr := "4:spam3:egg"
+	strrr := "4:spam3:eggi42e"
 	i := 0
-	res := ""
+	var res []any
 	for i < len(strrr) {
-		res += decodeString(strrr, &i)
+		r := rune(strrr[i])
+		if unicode.IsDigit(r) {
+			res = append(res, decodeString(strrr, &i))
+		} else if r == 'i' {
+			res = append(res, decodeInteger(strrr, &i))
+		} else {
+			i++
+		}
 	}
 	fmt.Println(res)
+}
+
+func decodeInteger(s string, i *int) int {
+	res := ""
+	*i++ // skip i
+	for string(s[*i]) != "e" {
+		res += string(s[*i])
+		*i++
+	}
+	resInt, err := strconv.Atoi(res)
+	fmt.Println(res)
+	if err != nil {
+		log.Fatal("invalid bencode")
+	}
+	return resInt
 }
 
 func decodeString(s string, i *int) string {
@@ -26,11 +48,11 @@ func decodeString(s string, i *int) string {
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		*i++
+		*i++ //move to colon
 		if s[*i] != ':' {
 			log.Fatal("expected colon")
 		}
-		*i++
+		*i++ // skip colon
 		strParsed := s[*i : *i+length]
 		res += strParsed
 	} else {
