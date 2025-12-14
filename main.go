8eb9f85937ca8f6044570f182d78a150382b6457
ito <ito.k23018@gmail.com>
2025-12-14 17:51:38 +0530

    refactoring for github

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fileData, err := os.ReadFile("a.torrent")
+	fileData, err := os.ReadFile("torrent_files/a.torrent")
 	if err != nil {
 		log.Fatal("cannot read file")
 		log.Fatal(err.Error())
@@ -20,7 +20,18 @@ func main() {
 	if !ok {
 		log.Fatal("tch tch tch")
 	}
-	fmt.Println(resMap["info"])
+	info, ok := resMap["info"].(map[string]any)
+	if !ok {
+		log.Fatal("tch tch info")
+	}
+	pieces := info["pieces"].(string)
+	fmt.Println([]byte(pieces))
+
+	// list := resMap["announce-list"].([]string) // we cannt assert to []strinf
+	// list := resMap["announce-list"].([]any)
+	// for _, val := range list {
+	// 	fmt.Println(val)
+	// }
 }
 
 // dic := "d3:cow3:moo4:spam4:eggs3:numi42e4:listl4:test3:one3:twoe3:fooi99ee"
